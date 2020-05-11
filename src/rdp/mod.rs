75d276fb2082b6056f4576b4bcfd37019319913e
David Young <david.young@nccgroup.com>
2020-05-11 10:24:21 +0100

    Add GPL-3 license text

--- src/rdp/mod.rs
+++ src/rdp/mod.rs
@@ -1,3 +1,22 @@
+/*
+ *   This file is part of NCC Group Scamper https://github.com/nccgroup/scamper
+ *   Copyright 2020 David Young <david(dot)young(at)nccgroup(dot)com>
+ *   Released as open source by NCC Group Plc - https://www.nccgroup.com
+ *
+ *   Scamper is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Scamper is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Scamper.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use crate::parsing::Target;
 use crate::util::target_to_filename;
 use image::{DynamicImage, ImageBuffer, Rgba};
